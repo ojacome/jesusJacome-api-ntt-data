2e03436ed669b786dd5b573c996c21fc7cf4d694
ojacome01 <ojacome@libelulasoft.com>
2022-08-14 20:03:49 -0500

    add env

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,12 +1,12 @@
+require('dotenv').config();
 import { DataSource } from "typeorm";
 import { URL } from "url";
-import { DB_URL } from "./config/environment"
 import { Organization } from './entity/Organization';
 import { Tribe } from './entity/Tribe';
 import { Metric } from './entity/Metric';
 import { Repository } from './entity/Repository';
 
-const dbUrl = new URL(DB_URL);
+const dbUrl = new URL(process.env.DATABASE_URL || "");
 const routingId = dbUrl.searchParams.get("options");
 dbUrl.searchParams.delete("options");
 
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 // import 'reflect-metada'
+require('dotenv').config();
 import express, { NextFunction, Request, Response } from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import { AppDataSource } from './dataSource'
-import { Organization } from './entity/Organization'
-import { Repository, StateRepository, StatusRepository } from './entity/Repository'
-import { Metric } from './entity/Metric'
-import { Tribe } from './entity/Tribe'
 import router from './routes/router'
 
 let app = express()
-
+let port = Number(process.env.PORT) || 3000
 //middlewares
 app.use(cors())
 app.use(morgan('dev'))
@@ -28,38 +25,11 @@ app.use((err:Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ message: 'Error en el servidor!'});
 });
 
-app.listen(3000, () => console.log('server on port ', 3000))
+app.listen(port, () => console.log('server on port ', port))
 
 
 AppDataSource.initialize()
     .then(async () => {
-        // let metric = new Metric
-        // metric.bugs = 8
-        // metric.code_smells = 3
-        // metric.coverage = 50
-        // metric.hotspot = 2
-        // metric.vulnerabilities = 7
-        // await AppDataSource.manager.save(metric)
-
-        // let repo = new Repository
-        // repo.name = "backend"
-        // repo.state = StateRepository.DISABLE
-        // repo.status = StatusRepository.ACTIVE
-        // repo.metric = metric
-        // await AppDataSource.manager.save(repo)
-
-        // let tribe = new Tribe
-        // tribe.name = "adq :')"
-        // tribe.status = 1
-        // tribe.repositories = [repo]
-        // await AppDataSource.manager.save(tribe)
-
-        // let organization = new Organization
-        // organization.name = "ntt data"
-        // organization.status = 1
-        // organization.tribes = [tribe]
-        // await AppDataSource.manager.save(organization)
-
         console.log("Data Source has been initialized!")
     })
     .catch((err) => {
